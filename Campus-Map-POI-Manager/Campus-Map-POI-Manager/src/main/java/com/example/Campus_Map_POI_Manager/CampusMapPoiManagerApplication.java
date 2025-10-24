fb1b45cac672f5aa38982a60a3e9649ea8925805
Logaprabhu75 <logaprabhu549@gmail.com>
2025-10-24 20:52:58 +0530

    Add files via upload
    folders update

---
+++ Campus-Map-POI-Manager/Campus-Map-POI-Manager/src/main/java/com/example/Campus_Map_POI_Manager/CampusMapPoiManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Campus_Map_POI_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusMapPoiManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusMapPoiManagerApplication.class, args);
+	}
+
+}
---
+++ Campus-Map-POI-Manager/Campus-Map-POI-Manager/src/test/java/com/example/Campus_Map_POI_Manager/CampusMapPoiManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Campus_Map_POI_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusMapPoiManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
